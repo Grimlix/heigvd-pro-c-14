c1045256a964038fb8d971ef6644b01fad41875f
Simon Flückiger <simon.fluckiger@hotmail.com>
2020-04-06 19:09:19 +0200

    add squelet (with files)

---
+++ src/Controller/User_controller.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class User_controller extends AbstractController
+{
+    public function getCurrentPoll()
+    {
+        $number = random_int(100, 1000);
+
+        return new Response(
+            '<html><body>Lucky number: ' . $number . '</body></html>'
+        );
+    }
+
+}
