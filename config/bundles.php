1d917d664c6aec669bc56f3fe1d838308bd5b667
Grimlix <nic.hungi5@gmail.com>
2020-05-09 15:06:45 +0200

    Added a run button to easyAdmin allowing to start the poll

--- config/bundles.php
+++ config/bundles.php
@@ -13,4 +13,5 @@ return [
     FOS\UserBundle\FOSUserBundle::class => ['all' => true],
     EasyCorp\Bundle\EasyAdminBundle\EasyAdminBundle::class => ['all' => true],
     Symfony\Bundle\MercureBundle\MercureBundle::class => ['all' => true],
+    Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle::class => ['all' => true],
 ];
